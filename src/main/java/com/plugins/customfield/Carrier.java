1abc1fd0eda0a89fe7bc6e4c76c76265c92c0337
Aleksei Sapunov <aleksei.sapunov@modirum.com>
2021-11-02 15:07:25 +0300

    Added gitignore, removed folders

--- src/main/java/com/plugins/customfield/Carrier.java
+++ src/main/java/com/plugins/customfield/Carrier.java
@@ -1,7 +1,6 @@
 package com.plugins.customfield;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 
 /**
  * This is the singular object for this custom field type.
@@ -10,12 +9,14 @@ import java.util.ArrayList;
  */
 public class Carrier {
 
-    private final Double fullAmount, advance, willingness, rate ;
-    private final int  daysAdvance, daysWillingness;
     public static final int NUMBER_OF_VALUES = 6;
     public static final int NUMBER_OF_INT_VALUES = 2;
     public static final int NUMBER_OF_DOUBLE_VALUES = 4;
-    public Carrier( Double fullAmount, Double rate, Double advance,  Double willingness, int daysAdvance, int daysWillingness) {
+
+    private final Double fullAmount, advance, willingness, rate;
+    private final int daysAdvance, daysWillingness;
+
+    public Carrier(Double fullAmount, Double rate, Double advance, Double willingness, int daysAdvance, int daysWillingness) {
         this.fullAmount = fullAmount;
         this.advance = advance;
         this.willingness = willingness;
@@ -65,20 +66,10 @@ public class Carrier {
         return rate;
     }
 
-    public String getAnswer()
-    {
+    public String getAnswer() {
         DecimalFormat dF = new DecimalFormat("#.##");
-        Double ans= advance*rate/365*daysAdvance+willingness*rate/365*daysWillingness;
+        Double ans = advance * rate / 365 * daysAdvance + willingness * rate / 365 * daysWillingness;
         return dF.format(ans);
     }
 
-   /* public ArrayList<Double> getCarrierValues(){
-        ArrayList<Double> allValues = new ArrayList<Double>();
-        allValues.add(fullAmount);
-        allValues.add(advance);
-        allValues.add(daysAdvance);
-        allValues.add(daysWillingness);
-        allValues.add(rate);
-        return allValues;
-    }*/
 }
