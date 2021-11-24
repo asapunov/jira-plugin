2b8586fba039203d1226f1f9490ac957e14e36b4
Pan4ick <grimasa904@gmail.com>
2021-11-24 10:44:58 +0300

    fixes

--- src/main/resources/js/accountCF/jira-custom-field.js
+++ src/main/resources/js/accountCF/jira-custom-field.js
@@ -3,10 +3,8 @@ function createInput(){
     let a = "${field_uid}";
     i++;
     var $input = $("<tr>" + "<td>" + "<input  type=\"text\" value=\"\" name=\"" + a + "\" id=\"" + a + " new_f" + i + "\" /> "  +
-        " #datePopup( " + a + " new_f" + i + " \"  $dateTimeFormat $dateFormat $timeFormat $dateTimePicker $currentMillis $currentCalendar)" +
         "<td>" + "<input  type=\"text\" value=\"\" name=\"" + a + "\" id=\"" + a + " new_s" + i + "\" /> " +
         "<td>" + "<input  type=\"text\" value=\"\" name=\"" + a + "\" id=\"" + a + " new_t" + i + "\" /> " +
-        " #datePopup( " + a + " new_t" + i + "   $dateTimeFormat $dateFormat $timeFormat $dateTimePicker $currentMillis $currentCalendar)" +
         "<td>" + "<input  type=\"text\" value=\"\" name=\"" + a + "\" id=\"" + a + " new_f" + i + "\" /> "+ "</tr>");
     $input.appendTo('#mainTab');
 }
