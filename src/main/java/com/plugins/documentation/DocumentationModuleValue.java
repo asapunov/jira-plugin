4e8987ac32279ad61ed056e311adb1e3677d347e
Alexander <grimasa904@gmail.com>
2022-06-27 02:01:15 +0300

    Supplier's offer realization

--- src/main/java/com/plugins/documentation/DocumentationModuleValue.java
+++ src/main/java/com/plugins/documentation/DocumentationModuleValue.java
@@ -1,37 +1,70 @@
 package com.plugins.documentation;
 
+import com.atlassian.jira.bc.issue.IssueService;
 import com.atlassian.jira.component.ComponentAccessor;
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.issue.*;
+import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.util.velocity.NumberTool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+
+import java.util.*;
 
 public class DocumentationModuleValue extends AbstractJiraContextProvider {
 
     private static final Logger log = LoggerFactory.getLogger(DocumentationModuleValue.class);
     private static final IssueManager issueManager = ComponentAccessor.getIssueManager();
+    private static final ApplicationUser currentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
+    private static final IssueService issueService = ComponentAccessor.getIssueService();
+    private static final CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager();
     private static final NumberTool numberTool = new NumberTool(new Locale("ru", "RU"));
     @Override
     public Map<String, Object> getContextMap(ApplicationUser applicationUser, JiraHelper jiraHelper) {
         Map<String, Object> contextMap = new HashMap<>();
         Issue issue = (Issue)jiraHelper.getContextParams().get("issue");
-        contextMap.put("customFieldManager", ComponentAccessor.getCustomFieldManager());
+        contextMap.put("customFieldManager", customFieldManager);
         contextMap.put("issueManager", issueManager);
         contextMap.put("issue", issue);
         contextMap.put("number", numberTool);
         contextMap.put("baseURL", getjiraURL());
+        contextMap.put("supplierOffers", getSupplierOffers(issue));
         return contextMap;
     }
 
-/*    private String getCfValue(String cfNumber, Issue issue) {
-        CustomField customField = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(cfNumber);
+    private IssueService.IssueResult validation(Issue issue, int actionId){
+        IssueService.TransitionValidationResult transitionValidationResult = issueService.validateTransition(currentUser, issue.getId(), actionId, new IssueInputParametersImpl());
+        if (transitionValidationResult.isValid()) {
+            IssueService.IssueResult transitionResult = issueService.transition(currentUser, transitionValidationResult);
+            if (transitionResult.isValid())
+                log.debug("Transitioned issue" + issue + "through action $actionId");
+            else
+                log.debug("Transition result is not valid");
+            return transitionResult;
+        }
+        else {
+            log.debug("The transitionValidation is not valid");
+            return null;
+        }
+
+    }
+
+    private ArrayList<Issue> getSupplierOffers(Issue issue){
+        String supplierOffers = getCfValue("Предложения поставщиков", issue);
+        assert supplierOffers != null;
+        String[] offers = supplierOffers.split(",");
+        ArrayList<Issue> issuesSO = new ArrayList<>();
+        for (String issues: offers){
+            issues = issues.trim();
+            issuesSO.add(issueManager.getIssueObject(issues));
+        }
+        return issuesSO;
+    }
+
+    private String getCfValue(String cfName, Issue issue) {
+        CustomField customField = customFieldManager.getCustomFieldObjectByName(cfName);
         try {
             String cfValue = (customField != null ? customField.getValue(issue).toString() : null);
             log.warn(cfValue);
@@ -41,7 +74,7 @@ public class DocumentationModuleValue extends AbstractJiraContextProvider {
             log.warn("Field is empty");
             return null;
         }
-    }*/
+    }
 
     private String getjiraURL() {
         return ComponentAccessor.getApplicationProperties().getString("jira.baseurl");
