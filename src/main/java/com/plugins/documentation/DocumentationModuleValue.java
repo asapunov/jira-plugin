4ac93c2ad477fc1bc53c9dbc0644ac6b4f5ab135
Alexander <grimasa904@gmail.com>
2022-07-04 01:18:20 +0300

    fixes

--- src/main/java/com/plugins/documentation/DocumentationModuleValue.java
+++ src/main/java/com/plugins/documentation/DocumentationModuleValue.java
@@ -6,12 +6,18 @@ import com.atlassian.jira.issue.*;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
+import com.atlassian.jira.rest.client.api.IssueRestClient;
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Transition;
+import com.atlassian.jira.rest.client.api.domain.input.TransitionInput;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.util.velocity.NumberTool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+import java.net.URI;
 import java.util.*;
 
 public class DocumentationModuleValue extends AbstractJiraContextProvider {
@@ -32,6 +38,7 @@ public class DocumentationModuleValue extends AbstractJiraContextProvider {
         contextMap.put("number", numberTool);
         contextMap.put("baseURL", getjiraURL());
         contextMap.put("supplierOffers", getSupplierOffers(issue));
+        contextMap.put("DMV", this);
         return contextMap;
     }
 
@@ -45,6 +52,54 @@ public class DocumentationModuleValue extends AbstractJiraContextProvider {
             issuesSO.add(issueManager.getIssueObject(issues));
         }
         return issuesSO;
+    }
+
+    public IssueService.IssueResult validation(Issue issue, int actionId){
+        IssueInputParameters issueInputParameters = new IssueInputParametersImpl();
+        IssueService.TransitionValidationResult transitionValidationResult = issueService.validateTransition(currentUser, issue.getId(), actionId, issueInputParameters);
+        if (transitionValidationResult.isValid()) {
+            IssueService.IssueResult transitionResult = issueService.transition(currentUser, transitionValidationResult);
+            if (transitionResult.isValid())
+                log.debug("Transitioned issue" + issue + "through action $actionId");
+            else
+                log.debug("Transition result is not valid");
+            return transitionResult;
+        }
+        else {
+            log.debug("The transitionValidation is not valid");
+            return null;
+        }
+
+    }
+
+    public void updateIssueStatus(Issue issue, String status) {
+        IssueRestClient client = getJiraRestClient().getIssueClient();
+
+        Iterable<Transition> transitions = client.getTransitions((com.atlassian.jira.rest.client.api.domain.Issue) issue).claim();
+
+        for(Transition t : transitions){
+            if(t.getName().equals(status)) {
+                TransitionInput input = new TransitionInput(t.getId());
+                client.transition((com.atlassian.jira.rest.client.api.domain.Issue) issue, input).claim();
+                return;
+            }
+        }
+    }
+
+
+// you can ignore below if you know how to get IssueRestClient and Issue
+
+    public JiraRestClient getJiraRestClient() {
+        AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+
+        JiraRestClient client = factory.createWithBasicHttpAuthentication(URI.create(getjiraURL()), "admin", "admin");
+
+        return client;
+    }
+
+    public Issue getIssue(String issueKey) {
+        IssueRestClient client = getJiraRestClient().getIssueClient();
+        return (Issue) client.getIssue(issueKey).claim();
     }
 
     private String getCfValue(String cfName, Issue issue) {
