6d20f9b05d18d9c7a90943fce0814e87a15e24b5
Pan4ick <grimasa904@gmail.com>
2022-07-29 11:22:00 +0300

    ScreenField

--- src/main/java/com/enviogroup/plugins/statusscreen/StatusScreen.java
+++ src/main/java/com/enviogroup/plugins/statusscreen/StatusScreen.java
@@ -1,22 +1,33 @@
 package com.enviogroup.plugins.statusscreen;
 
+import com.atlassian.jira.bc.issue.IssueService;
 import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.jira.issue.fields.screen.FieldScreen;
 import com.atlassian.jira.issue.fields.screen.FieldScreenManager;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
 import com.atlassian.jira.template.soy.SoyTemplateRendererProvider;
 import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.util.velocity.NumberTool;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.enviogroup.plugins.documentation.IssueWorker;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class StatusScreen extends AbstractJiraContextProvider {
 
     @ComponentImport
     private final FieldScreenManager fieldScreenManager;
+    private static final IssueManager issueManager = ComponentAccessor.getIssueManager();
+    private static final ApplicationUser currentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
+    private static final IssueService issueService = ComponentAccessor.getIssueService();
+    private static final CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager();
+    private static final NumberTool numberTool = new NumberTool(new Locale("ru", "RU"));
 
     public StatusScreen(FieldScreenManager fieldScreenManager) {
         this.fieldScreenManager = fieldScreenManager;
@@ -33,6 +44,12 @@ public class StatusScreen extends AbstractJiraContextProvider {
         Issue issue = (Issue) jiraHelper.getContextParams().get("issue");
         FieldScreen fieldScreen = getFieldScreen(10200);
         contextMap.put("fieldScreen", fieldScreen);
+        contextMap.put("issueManager", issueManager);
+        contextMap.put("currentUser", currentUser);
+        contextMap.put("issueService", issueService);
+        contextMap.put("customFieldManager", customFieldManager);
+        contextMap.put("number", numberTool);
+        contextMap.put("issue", issue);
         contextMap.put("soyRenderer", ComponentAccessor.getComponent(SoyTemplateRendererProvider.class).getRenderer());
         return contextMap;
     }
