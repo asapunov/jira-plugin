7b65764701ffc32b7e6ed20fe96a25237762bd6b
Pan4ick <grimasa904@gmail.com>
2022-08-25 13:00:39 +0300

    Entity Converter

--- src/main/java/com/enviogroup/plugins/status/screen/StatusScreen.java
+++ src/main/java/com/enviogroup/plugins/status/screen/StatusScreen.java
@@ -1,19 +1,11 @@
 package com.enviogroup.plugins.status.screen;
 
-import com.atlassian.jira.bc.issue.IssueService;
 import com.atlassian.jira.component.ComponentAccessor;
-import com.atlassian.jira.issue.CustomFieldManager;
-import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.IssueManager;
-import com.atlassian.jira.issue.fields.screen.FieldScreen;
-import com.atlassian.jira.issue.fields.screen.FieldScreenManager;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
 import com.atlassian.jira.template.soy.SoyTemplateRendererProvider;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.util.velocity.NumberTool;
-import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
-import com.enviogroup.plugins.documentation.IssueWorker;
 
 import java.util.HashMap;
 import java.util.Locale;
