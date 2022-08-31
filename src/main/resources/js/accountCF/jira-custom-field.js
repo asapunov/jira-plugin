8c7ea3c8d6b6fe97b6b253014b27cb9e44ed20ac
Aleksei Sapunov <aleksei.sapunov@modirum.com>
2022-08-31 18:47:41 +0300

    Добавлен тултип для статуса организации договора

--- src/main/resources/js/accountCF/jira-custom-field.js
+++ src/main/resources/js/accountCF/jira-custom-field.js
@@ -1,1 +1,8 @@
-
+window.onload = function() {
+    AJS.$('[data-tooltip]').tooltip({
+        title: function () {
+            return this.getAttribute("data-tooltip");
+        },
+        gravity: 'w'
+    });
+};
