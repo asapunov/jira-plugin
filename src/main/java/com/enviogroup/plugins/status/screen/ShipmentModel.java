1adc9c2f3eb2b40c2dcd9f8d68810e29e8f019c3
Aleksei Sapunov <aleksei.sapunov@modirum.com>
2022-10-11 13:52:50 +0300

    Bug fixes

--- src/main/java/com/enviogroup/plugins/status/screen/ShipmentModel.java
+++ src/main/java/com/enviogroup/plugins/status/screen/ShipmentModel.java
@@ -1,5 +1,9 @@
 package com.enviogroup.plugins.status.screen;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ShipmentModel extends IssueModel {
     private OrganisationModel organisation;
 
--- src/main/resources/js/dashboard-item-tenders.js
+++ src/main/resources/js/dashboard-item-tenders.js
@@ -69,7 +69,7 @@ define('dashboard-items/tenders', ['underscore', 'jquery', 'wrm/context-path'], 
 
                     $element.empty().html(Dashboard.Item.Tenders.Templates.IssueList({
                         issues: self.dataModelIssues.reduce(function(result, value, index, array) {
-                            if (index % 3 === 0)
+                            if (index % 2 === 0)
                                 result.push(array.slice(index, index + 2));
                             return result;
                         }, []),
@@ -92,7 +92,7 @@ define('dashboard-items/tenders', ['underscore', 'jquery', 'wrm/context-path'], 
         return $.ajax({
             method: "GET",
             //url: contextPath() + "/rest/api/2/search?jql=project %3D CRM AND issuetype = Тендеры AND Status changed from \"Подготовка документации\" to \"Тендер подан\" during (-" + preferences['due-date-input'] + ", now()) "
-            url: contextPath() + "/rest/api/2/search?maxResults=1000&jql=project %3D CRM AND issuetype = Тендеры AND (status = Выиграли OR status = \"Контракт закрыт\") AND \"Дата подведения итогов\" >= \"2022/01/01\" ORDER BY cf[10049] ASC"
+            url: contextPath() + "/rest/api/2/search?fields=key&maxResults=1000&jql=project %3D CRM AND issuetype = Тендеры AND (status = Выиграли OR status = \"Контракт закрыт\") AND \"Дата подведения итогов\" >= \"2022/01/01\" ORDER BY cf[10049] ASC"
         });
     };
 
