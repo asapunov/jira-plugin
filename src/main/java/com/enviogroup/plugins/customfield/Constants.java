28eb278ade2b795e32fc0a9b85b1d9bfd7486d52
Aleksei Sapunov <aleksei.sapunov@modirum.com>
2024-03-28 14:07:39 +0300

    Добавлена отметка о позднем согласовании невыхода

---
+++ src/main/java/com/enviogroup/plugins/customfield/Constants.java
@@ -0,0 +1,5 @@
+package com.enviogroup.plugins.customfield;
+
+public class Constants {
+    public static final long SYSTEM_LABLES_FIELD = 13121L;
+}
--- src/main/java/com/enviogroup/plugins/panel/tender/RightPanel.java
+++ src/main/java/com/enviogroup/plugins/panel/tender/RightPanel.java
@@ -1,25 +1,42 @@
 package com.enviogroup.plugins.panel.tender;
 
+
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.label.Label;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
 import com.atlassian.jira.template.soy.SoyTemplateRendererProvider;
 import com.atlassian.jira.user.ApplicationUser;
+import com.enviogroup.plugins.customfield.Constants;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static com.enviogroup.plugins.status.screen.CustomField.CUSTOM_FIELD_11411;
+import java.util.TreeSet;
 
 public class RightPanel extends AbstractJiraContextProvider {
+
     @Override
     public Map getContextMap(ApplicationUser applicationUser, JiraHelper jiraHelper) {
         Map<String, Object> contextMap = new HashMap<>();
         Issue issue = (Issue) jiraHelper.getContextParams().get("issue");
+
         contextMap.put("issue", issue);
         contextMap.put("baseURL", getJiraURL());
         contextMap.put("soyRenderer", ComponentAccessor.getComponent(SoyTemplateRendererProvider.class).getRenderer());
+
+        try {
+            CustomField customField = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(Constants.SYSTEM_LABLES_FIELD);
+            TreeSet<Label> treeSet = (TreeSet<Label>) issue.getCustomFieldValue(customField);
+            for (Label label : treeSet) {
+                if ("latenottogo".equalsIgnoreCase(label.getLabel())){
+                    contextMap.put("labelnottogo", true);
+                }
+            }
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+        }
         return contextMap;
     }
 
